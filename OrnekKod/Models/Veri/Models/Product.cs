8e92e403c37e59f64deccf6c5353f180bdcb54ce
Tuğba Çelik <tgb.celik02@gmail.com>
2015-09-13 12:08:15 +0300

    örnek kod git version 1

---
+++ OrnekKod/Models/Veri/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using OrnekKod.Models.Veri.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrnekKod.Models.Veri.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ OrnekKod/OrnekKod/Models/Veri/Context/KodDBContext.cs
@@ -0,0 +1,58 @@
+﻿using OrnekKod.Models.Veri.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OrnekKod.Models.Veri.Context
+{
+    public class KodDBContext:DbContext{
+
+
+
+        public KodDBContext() {
+
+         Database.Connection.ConnectionString = "server=TUGBAPC; database=ornek; Trusted_Connection=true;";
+          
+        }
+
+        
+        public DbSet <Product> Products { get; set; }
+         public void ekle()
+        {
+            KodDBContext entity = new KodDBContext();
+
+            Product product1 = new Product() { Name = "kalem",  DisplayOrder = "yes" };
+            Product product2 = new Product() { Name = "silgi",  DisplayOrder = "yes"  };
+            Product product3 = new Product() { Name = "defter", DisplayOrder = "yes" };
+            Product product4 = new Product() { Name = "çanta",  DisplayOrder = "yes" };
+
+            entity.Products.Add(product1);
+            entity.Products.Add(product2);
+            entity.Products.Add(product3);
+            entity.Products.Add(product4);
+            entity.SaveChanges();
+
+        }
+
+          
+              
+       
+
+            
+    
+  
+        
+        
+        }
+
+    }
+
+
+           
+    
+     
+
+
