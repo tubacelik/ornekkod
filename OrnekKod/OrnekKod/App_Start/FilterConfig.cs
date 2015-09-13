7cc4c3420ce5c2e0b2708986e7bb43f32f8d26ee
Tuğba Çelik <tgb.celik02@gmail.com>
2015-09-13 12:18:35 +0300

    no message

--- OrnekKod/OrnekKod/App_Start/FilterConfig.cs
+++ OrnekKod/OrnekKod/App_Start/FilterConfig.cs
@@ -8,6 +8,9 @@ namespace OrnekKod
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+
+
         }
     }
 }
